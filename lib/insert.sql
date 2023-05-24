1ae66fccc019e382d80582bd2269001291ac11fe
Rolex Omondi <rolexomondi46@gmail.com>
2023-05-24 18:52:18 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',4,'M','Brown','No',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',6,'M','Brown','Yes',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabith',9,'F','Black','No',False);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',8,'M','Pink','Yes',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',10,'F','Blue','No',False);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',2,'M','Brown','No',False);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',7,'F','Orange','Yes',True);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,1,'F','White','No',False);
